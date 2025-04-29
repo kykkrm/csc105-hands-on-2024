634236144b35ef5e1b9299995ab902409de1c641
kykkrm <nonlada.prem@mail.kmutt.ac.th>
2025-04-29 15:50:01 +0700

    week10

---
+++ week10/HW05-backend/csc105-HW05-67130500858-NonldaPream /src/types/index.ts
@@ -0,0 +1,13 @@
+export type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+export type CreateTodoInput = {
+  title: string;
+  completed?: boolean;
+};
+export type UpdateTodoInput = {
+  title?: string;
+  completed?: boolean;
+};
